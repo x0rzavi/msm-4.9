232e4247fe4ff6856f4a9a0990b5379ce1963bbd
Avishek Sen <x0rzavi@gmail.com>
2020-12-14 09:28:19 +0000

    test

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#0 SMP Mon Dec 14 09:27:28 UTC 2020"
+#define LINUX_COMPILE_BY "codespace"
+#define LINUX_COMPILE_HOST "codespaces_5c5e53"
+#define LINUX_COMPILER ""
